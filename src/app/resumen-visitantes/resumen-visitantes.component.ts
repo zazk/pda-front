1ce35eb4b9f1bdd00d9035eb1d1efae0acc099a8
Enrique Juan de Dios <juandedioz@gmail.com>
2018-06-07 22:12:53 -0500

    Modify Buttons for Printing and remove saved data

--- src/app/resumen-visitantes/resumen-visitantes.component.ts
+++ src/app/resumen-visitantes/resumen-visitantes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Pax } from "../models/pax";
+import { Router } from "@angular/router";
 
 declare var $: any;
 @Component({
@@ -12,7 +13,7 @@ export class ResumenVisitantesComponent implements OnInit {
   fecha: string;
   ruta: number;
   rutaActiva: string;
-  constructor() {}
+  constructor( private router: Router) {}
 
   ngOnInit() {
     this.paxes = JSON.parse(localStorage.getItem("paxes"));
@@ -20,6 +21,10 @@ export class ResumenVisitantesComponent implements OnInit {
     this.ruta = parseInt(localStorage.getItem("ruta"), 10);
     this.rutaActiva = JSON.parse( localStorage.getItem("rutas") )
       .find( obj => obj.srl_cod_ruta === this.ruta ).var_nombre;
+  }
+  onCreateGroup(): void {
+    "fecha,rutas,paxes,ruta".split(",").forEach(s => localStorage.removeItem(s));
+    this.router.navigate(["home"]);
   }
 }
 
