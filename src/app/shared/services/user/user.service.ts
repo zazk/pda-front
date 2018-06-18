9280d1ef46d1055e7406f5a9f64a16c69b02cd06
Enrique Juan de Dios <juandedioz@gmail.com>
2018-06-17 23:35:37 -0500

    Update Back-end Address

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -8,8 +8,8 @@ import { Grupo } from "../../../models/grupo";
 
 @Injectable()
 export class UserService {
-  public url: string = "//localhost:4007/";
-  // public url: string = "//ima.pe:8080/";
+  // public url: string = "//localhost:4007/";
+  public url: string = "//http://168.121.48.106:4007/";
 
   user: Subject<any> = new Subject();
   constructor(private http: HttpClient) {}
