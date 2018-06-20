357cb4d5b724fdc2ad45eda1e4e06ea9ac418a08
Enrique Juan de Dios <juandedioz@gmail.com>
2018-06-19 22:48:17 -0500

    Added Visitantes from Endpoint

--- src/app/grupos/grupos.component.ts
+++ src/app/grupos/grupos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Grupo } from "../models/grupo";
 import { Router } from "@angular/router";
+import { UserService } from "../shared/services/user/user.service";
 
 declare var $: any;
 @Component({
@@ -10,10 +11,28 @@ declare var $: any;
 })
 export class GruposComponent implements OnInit {
   grupos: Grupo[];
-  constructor( private router: Router) {}
+  constructor(private router: Router, private service: UserService) {}
 
   ngOnInit() {
     this.grupos = JSON.parse(localStorage.getItem("grupos")) || [];
+    console.log("USER", this.service.theUser);
+    const user = JSON.parse(this.service.theUser);
+    this.service
+      .consultaGrupooperador(user.var_cod_operador)
+      .subscribe((grupos: any[]) => {
+        this.grupos = grupos.map(obj => {
+          return new Grupo(
+            [],
+            obj.srl_cod_grupo,
+            obj.dte_fec_programada,
+            obj.srl_cod_ruta,
+            obj.num_costo,
+            obj.var_cod_operador,
+            obj.int_nro_visitante
+          );
+        });
+        console.log("GRUPOS?", grupos);
+      });
   }
   onVerGrupo(grupo: Grupo) {
     console.log("Grupo:", grupo);
