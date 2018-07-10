6bdaf077818b420fe77a506af8e6167178d105af
Enrique Juan de Dios <juandedioz@gmail.com>
2018-07-10 09:53:37 -0500

    Added Hash to Routes to prevent root routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,92 +1,92 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MaterialModule } from "./material.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
 
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { UserService } from "./shared/services/user/user.service";
-import { AuthGuard } from "./shared/auth/auth.guard";
-import { LoginComponent } from "./login/login.component";
-import { IngresoVisitantesComponent } from "./ingreso-visitantes/ingreso-visitantes.component";
-import { ConfirmarVisitantesComponent } from "./confirmar-visitantes/confirmar-visitantes.component";
-import { ResumenVisitantesComponent } from "./resumen-visitantes/resumen-visitantes.component";
-import { GruposComponent } from "./grupos/grupos.component";
-import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
-import { PagosComponent } from "./pagos/pagos.component";
-import { NoticiasComponent } from "./noticias/noticias.component";
-import { CambioClaveComponent } from "./cambio-clave/cambio-clave.component";
-import { VerVisitantesComponent } from "./ver-visitantes/ver-visitantes.component";
-import { AgregarPagoComponent } from "./agregar-pago/agregar-pago.component";
-import { UpdateVisitantesComponent } from "./components/update-visitantes/update-visitantes.component";
-import { UpdateGrupoComponent } from "./components/update-grupo/update-grupo.component";
-import { ModifyGrupoComponent } from "./components/modify-grupo/modify-grupo.component";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserService } from './shared/services/user/user.service';
+import { AuthGuard } from './shared/auth/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { IngresoVisitantesComponent } from './ingreso-visitantes/ingreso-visitantes.component';
+import { ConfirmarVisitantesComponent } from './confirmar-visitantes/confirmar-visitantes.component';
+import { ResumenVisitantesComponent } from './resumen-visitantes/resumen-visitantes.component';
+import { GruposComponent } from './grupos/grupos.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { VerVisitantesComponent } from './ver-visitantes/ver-visitantes.component';
+import { AgregarPagoComponent } from './agregar-pago/agregar-pago.component';
+import { UpdateVisitantesComponent } from './components/update-visitantes/update-visitantes.component';
+import { UpdateGrupoComponent } from './components/update-grupo/update-grupo.component';
+import { ModifyGrupoComponent } from './components/modify-grupo/modify-grupo.component';
 
 const AppRouter: Routes = [
   {
-    path: "login",
+    path: 'login',
     component: LoginComponent
   },
   {
-    path: "",
-    redirectTo: "home",
-    pathMatch: "full"
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path: "home",
+    path: 'home',
     component: HomeComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "cambio-clave",
+    path: 'cambio-clave',
     component: CambioClaveComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "ingreso-visitantes",
+    path: 'ingreso-visitantes',
     component: IngresoVisitantesComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "confirmar-visitantes",
+    path: 'confirmar-visitantes',
     component: ConfirmarVisitantesComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "resumen-visitantes",
+    path: 'resumen-visitantes',
     component: ResumenVisitantesComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "ver-visitantes/:codigo",
+    path: 'ver-visitantes/:codigo',
     component: VerVisitantesComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "modificar-grupo/:codigo",
+    path: 'modificar-grupo/:codigo',
     component: ModifyGrupoComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "grupos",
+    path: 'grupos',
     component: GruposComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "pagos",
+    path: 'pagos',
     component: PagosComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "agregar-pago",
+    path: 'agregar-pago',
     component: AgregarPagoComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "noticias",
+    path: 'noticias',
     component: NoticiasComponent,
     canActivate: [AuthGuard]
   }
@@ -118,7 +118,7 @@ const AppRouter: Routes = [
     HttpClientModule,
     MaterialModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(AppRouter, { enableTracing: true })
+    RouterModule.forRoot(AppRouter, { enableTracing: true, useHash: true })
   ],
   providers: [AuthGuard, UserService],
   bootstrap: [AppComponent]
